a687676d307bdb69371f73d4321390abc3350262
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-10-12 11:56:07 +0700

    Add Deadlock Avoidance demo

---
+++ Deadlock/DeadlockAvoidance.py
@@ -0,0 +1,64 @@
+from Deadlock.SystemInfo import SystemInfo, CustomList
+
+
+def deadlock_avoidance():
+    choices = ['Banker\'s algorithm check', 'Resource-Request algorithm']
+    print('Choose the demo you want!\n' + '\n'.join(
+        [f'[{i + 1}] {c}' for i, c in enumerate(choices)]))
+    c = -1
+    while not 0 <= c < len(choices):
+        c = int(input('Enter your choice (1): ') or 1) - 1
+
+    input_ways = ['Typing each items', 'Choose a file']
+    print('Choose input way\n' + '\n'.join([f'[{i + 1}] {input_way}' for i, input_way in enumerate(input_ways)]))
+    input_way_index = 0
+    while not (1 <= input_way_index <= 2 or input_way_index is None):
+        input_way_index = int(input('Input way (2): ') or 2)
+    number_of_processes = 5
+    number_of_resource_types = 3
+    allocation = [CustomList([0, 1, 0]), CustomList([2, 0, 0]), CustomList([3, 0, 2]), CustomList([2, 1, 1]),
+                  CustomList([0, 0, 2])]
+    claim = [CustomList([7, 5, 3]), CustomList([3, 2, 2]), CustomList([9, 0, 2]), CustomList([2, 2, 2]),
+             CustomList([4, 3, 3])]
+    available = CustomList([3, 3, 2])
+    number_of_requests = 0
+    requests = []
+    process_indexes = []
+    if input_way_index == 1:
+        number_of_processes = int(input("Number of processes:") or number_of_processes)
+        number_of_resource_types = int(input("Number of resource types:") or number_of_resource_types)
+        allocation = [CustomList(map(int, input(f"Allocated resources of process {i}:").split() or allocation[i])) for i in
+                      range(number_of_processes)]
+        claim = [CustomList(map(int, input(f"Maximum claim resources of process {i}:").split() or claim[i])) for i in
+                 range(number_of_processes)]
+        available = CustomList(map(int, input("Available resources: ").split() or available))
+        if c == 1:
+            number_of_requests = int(input("Number of requests:") or number_of_requests)
+            for i in range(number_of_requests):
+                index = int(input("Process:"))
+                process_indexes.append(index)
+                requests.append(CustomList(map(int, input(f"Request of process {index}:").split())))
+    elif input_way_index == 2:
+        file_name = input('[+] Filename (input_deadlock_avoidance.txt): ')
+        with open(file_name or 'input_deadlock_avoidance.txt', mode='r') as f:
+            number_of_processes, number_of_resource_types = list(map(int, f.readline().split()))
+            allocation = [CustomList(map(int, f.readline().split())) for _ in range(number_of_processes)]
+            claim = [CustomList(map(int, f.readline().split())) for _ in range(number_of_processes)]
+            available = CustomList(map(int, f.readline().split()))
+            if c == 1:
+                number_of_requests = int(f.readline())
+                for i in range(number_of_requests):
+                    index = int(f.readline())
+                    process_indexes.append(index)
+                    requests.append(CustomList(map(int, f.readline().split())))
+    system = SystemInfo(number_of_processes, number_of_resource_types, available, claim, allocation)
+    system.print_state()
+    if c == 0:
+        print(system.banker_algorithm())
+    if c == 1:
+        for (process_index, request) in zip(process_indexes, requests):
+            print(system.resources_request_algorithm(process_index, request))
+
+
+
+deadlock_avoidance()
---
+++ Deadlock/SystemInfo.py
@@ -0,0 +1,146 @@
+from typing import List
+from prettytable import PrettyTable
+
+
+class CustomList(List):
+    def __le__(self, other):
+        for i in range(min(len(other), len(self))):
+            if self[i] > other[i]:
+                return False
+        return True
+
+    def __sub__(self, other):
+        return CustomList([self[i] - other[i] for i in range(min(len(other), len(self)))])
+
+    def __add__(self, other):
+        return CustomList([self[i] + other[i] for i in range(min(len(other), len(self)))])
+
+    def __iadd__(self, other):
+        return CustomList([self[i] + other[i] for i in range(min(len(other), len(self)))])
+
+    def copy(self):
+        arr = super().copy()
+        return CustomList(arr)
+
+
+class SystemInfo:
+    def __init__(self, number_of_processes: int, number_of_resource_types: int, available: CustomList[int],
+                 claim: List[CustomList[int]], allocation: List[CustomList[int]]):
+        print(number_of_processes, number_of_resource_types, available, allocation, claim, sep='\n')
+        self._number_of_process = number_of_processes
+        self._number_of_resource_types = number_of_resource_types
+        self._available = available.copy()
+        # print(self._available.__class__)
+        self._claim = claim.copy()
+        self._allocation = allocation.copy()
+        self._need = [self._claim[i] - self._allocation[i] for i in range(number_of_processes)]
+        # print(self._claim)
+        # print(self._allocation)
+        # print(self._need)
+        self._work = available.copy()
+        # print(self._work.__class__)
+        self._finished = [False] * number_of_processes
+        self._requests = [CustomList([0] * number_of_resource_types) for _ in range(number_of_processes)]
+        self._print_banker_table = PrettyTable()
+        self._print_banker_table.field_names = ['Process', 'Claim', 'Allocation', 'Need', 'Available', 'New available']
+        self._current_state_table = PrettyTable()
+        self._current_state_table.field_names = ['Process', 'Claim', 'Allocation', 'Need']
+
+    def _find_process_not_finished(self) -> int:
+        for i in range(self._number_of_process):
+            if not self._finished[i] and self._need[i] < self._work:
+                return i
+
+        return -1
+
+    def _find_request_waiting(self) -> int:
+        for i in range(self._number_of_process):
+            if self._requests[i] != [0] * self._number_of_resource_types:
+                print(self._requests[i])
+                return i
+        print(-1)
+        return -1
+
+    def _pretend_allocation(self, i: int):
+        # print(f'Before: {self._available}, {self._allocation[i]}, {self._need[i]}')
+        self._available = self._available - self._requests[i]
+        self._allocation[i] = self._allocation[i] + self._requests[i]
+        self._need[i] = self._need[i] - self._requests[i]
+        # print(f'After: {self._available}, {self._allocation[i]}, {self._need[i]}')
+
+    def _restore_state(self, i: int):
+        self._available = self._available + self._requests[i]
+        self._allocation[i] = self._allocation[i] - self._requests[i]
+        self._need[i] = self._need[i] + self._requests[i]
+
+    def print_state(self):
+        self._current_state_table.clear_rows()
+        for i, (claim, allocation, need) in enumerate(zip(self._claim, self._allocation, self._need)):
+            self._current_state_table.add_row([i, claim, allocation, need])
+        print('Current state of system:', self._current_state_table, f'Available resources: {self._available}\n', sep='\n')
+
+    def banker_algorithm(self) -> dict:
+        self._print_banker_table.clear_rows()
+        processes = []
+        self._work = self._available.copy()
+        self._finished = [False] * self._number_of_process
+        process_index = 0
+        while process_index >= 0:
+            process_index = self._find_process_not_finished()
+            if process_index >= 0:
+                row_table = [process_index, self._claim[process_index], self._allocation[process_index],
+                             self._need[process_index], self._work]
+                self._work = self._work + self._allocation[process_index]
+                row_table.append(self._work)
+                self._print_banker_table.add_row(row_table)
+                # self._allocation[process_index] = CustomList([0] * self._number_of_resource_types)
+                # print(self._work)
+                self._finished[process_index] = True
+                processes.append(process_index)
+
+        for i in range(self._number_of_process):
+            if not self._finished[i]:
+                return {"safe": False, "processes": processes}
+
+        print(f'Banker\'s algorithm:\n {self._print_banker_table}')
+        return {"safe": True, "processes": processes}
+
+    def resources_request_algorithm(self, process_index: int, request: CustomList[int]):
+        self._requests[process_index] = request
+        ret = {"safe": False, "message": "Request accepted!"}
+        print(f'Process {process_index} requests {request}')
+        self.print_state()
+
+        if self._requests[process_index] <= self._need[process_index]:
+            if self._requests[process_index] <= self._available:
+                self._pretend_allocation(process_index)
+                if not self.banker_algorithm()["safe"]:
+                    ret["message"] = 'Request make system enter unsafe state'
+                    self._restore_state(process_index)
+                else:
+                    ret["safe"] = True
+                    self._requests[process_index] = CustomList([0] * self._number_of_resource_types)
+            else:
+                ret["message"] = "Request has exceeded available resources"
+
+        else:
+            ret["message"] = 'Request has exceeded the maximum claim'
+        return ret
+
+
+if __name__ == "__main__":
+    number_of_processes = 5
+    number_of_resource_types = 3
+    allocation = [CustomList([0, 1, 0]), CustomList([2, 0, 0]), CustomList([3, 0, 2]), CustomList([2, 1, 1]),
+                  CustomList([0, 0, 2])]
+    claim = [CustomList([7, 5, 3]), CustomList([3, 2, 2]), CustomList([9, 0, 2]), CustomList([2, 2, 2]),
+             CustomList([4, 3, 3])]
+    available = CustomList([3, 3, 2])
+    system = SystemInfo(number_of_processes, number_of_resource_types, available, claim, allocation)
+    print(system.banker_algorithm())
+    request = CustomList([1, 0, 2])
+    processes_index = 1
+    print(system.resources_request_algorithm(processes_index, request))
+    request = CustomList([3, 3, 0])
+    processes_index = 4
+    print(system.resources_request_algorithm(processes_index, request))
